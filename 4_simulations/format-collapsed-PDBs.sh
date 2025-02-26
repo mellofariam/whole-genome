c6c734b362afeaf32c238150dfa8901feb84fcc8
Matheus Mello <mfmello@outlook.com>
2025-02-26 15:01:34 -0600

    removed comments from bash script

--- 4_simulations/format-collapsed-PDBs.sh
+++ 4_simulations/format-collapsed-PDBs.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Iterate through all folders in the ../3_collapse directory
 for folder in ../3_collapse/*; do
     if [ -d "$folder" ]; then
         for chr in {1..46}; do
-            # Check if the file exists
             files=$(find "$folder" -name "chr${chr}_*.pdb")
             if [ -n "$files" ]; then
                 for file in $files; do
